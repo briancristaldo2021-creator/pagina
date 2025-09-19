715ce791c7344f6b6e82fb1674639d20c7187812
briancristaldo2021-creator <briancristaldo2021@gmail.com>
2025-09-19 18:01:00 -0300

    Add files via upload

---
+++ codigos.js
@@ -0,0 +1,47 @@
+// ==============================
+// Figuritas y Códigos QR centralizados
+// ==============================
+
+// Claves de QR => ID de figurita
+const codigosQR = {
+  "lcdm_fig1": "fig1",
+  "lcdm_fig2": "fig2",
+  "lcdm_fig3": "fig3",
+  "lcdm_fig4": "fig4",
+  "lcdm_fig5": "fig5"
+};
+
+// Lista de figuritas con sus imágenes
+// ?? Debes poner tus imágenes reales en /img/ (una en color y otra en gris)
+const figuritasData = [
+  {
+    id: "fig1",
+    nombre: "Figurita 1",
+    imgBloqueada: "img/fig1_gray.png",
+    imgColor: "img/fig1.png"
+  },
+  {
+    id: "fig2",
+    nombre: "Figurita 2",
+    imgBloqueada: "img/fig2_gray.png",
+    imgColor: "img/fig2.png"
+  },
+  {
+    id: "fig3",
+    nombre: "Figurita 3",
+    imgBloqueada: "img/fig3_gray.png",
+    imgColor: "img/fig3.png"
+  },
+  {
+    id: "fig4",
+    nombre: "Figurita 4",
+    imgBloqueada: "img/fig4_gray.png",
+    imgColor: "img/fig4.png"
+  },
+  {
+    id: "fig5",
+    nombre: "Figurita 5",
+    imgBloqueada: "img/fig5_gray.png",
+    imgColor: "img/fig5.png"
+  }
+];
