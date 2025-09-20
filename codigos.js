b70a9998b0288f176b973da17df266fca21736a5
briancristaldo2021-creator <briancristaldo2021@gmail.com>
2025-09-20 00:30:07 -0300

    Add files via upload

--- codigos.js
+++ codigos.js
@@ -13,9 +13,9 @@ const codigosQR = {
 
 // Lista de figuritas con sus imágenes
 const figuritasData = [
-  { id: "fig1", nombre: "Figurita 1", imgBloqueada: "img/fig1_gray.jpg", imgColor: "img/fig1.jpg" },
-  { id: "fig2", nombre: "Figurita 2", imgBloqueada: "img/fig2_gray.jpg", imgColor: "img/fig2.jpg" },
-  { id: "fig3", nombre: "Figurita 3", imgBloqueada: "img/fig3_gray.jpg", imgColor: "img/fig3.jpg" },
-  { id: "fig4", nombre: "Figurita 4", imgBloqueada: "img/fig4_gray.jpg", imgColor: "img/fig4.jpg" },
-  { id: "fig5", nombre: "Figurita 5", imgBloqueada: "img/fig5_gray.jpg", imgColor: "img/fig5.jpg" }
+  { id: "fig1", nombre: "Figurita 1", imgBloqueada: "img/fig1_gray.png", imgColor: "img/fig1.jpg" },
+  { id: "fig2", nombre: "Figurita 2", imgBloqueada: "img/fig2_gray.png", imgColor: "img/fig2.jpg" },
+  { id: "fig3", nombre: "Figurita 3", imgBloqueada: "img/fig3_gray.png", imgColor: "img/fig3.jpg" },
+  { id: "fig4", nombre: "Figurita 4", imgBloqueada: "img/fig4_gray.png", imgColor: "img/fig4.jpg" },
+  { id: "fig5", nombre: "Figurita 5", imgBloqueada: "img/fig5_gray.png", imgColor: "img/fig5.jpg" }
 ];
